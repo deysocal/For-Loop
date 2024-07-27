d0906c11f36513497ee0a5f5e02e6b7b8a92a526
deysocal <souravd@intellirevenue.com>
2024-07-28 02:58:51 +0530

    Todays Commit

--- Forloop.js
+++ Forloop.js
@@ -1,3 +1,5 @@
+
+/*For Loop */
 let j;
 
 for (j=1; j<=10; j++) {
@@ -5,4 +7,18 @@ if (j % 2 !==0)
 console.log("My name is Sourav", j)
 else 
 console.log("My name is Babai", j)
+}
+/*For in */
+let Cars=['Maruti','Hyundai','BMW'];
+let y
+for  (y in Cars)
+    {
+    console.log(Cars[y])
+}
+/*For of */
+let Country=['India','UK','FRANCE'];
+let Z
+for  (Z of Country)
+    {
+    console.log(Z)
 }
